512f0a15e9789670c4696ff3ab015bc1a80925b5
Ross Owen <ross@xmos.com>
2015-11-05 18:28:17 +0000

    Added XUD_TestMode.h/.xc

---
+++ lib_xud/src/core/XUD_TestMode.h
@@ -0,0 +1,11 @@
+#ifndef __XUD_TESTMODE_H__
+#define __XUD_TESTMODE_H__
+
+unsigned UsbTestModeHandler_asm();
+unsigned XUD_UsbTestSE0();
+int XUD_TestMode_TestJ () ;
+int XUD_TestMode_TestK () ;
+int XUD_TestMode_TestSE0NAK () ;
+int XUD_TestMode_TestPacket () ;
+
+#endif
